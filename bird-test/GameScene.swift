db303ede3c2ecb9bf4b6de3f61d7a4f34faf876e
vproper <bengboxer@gmail.com>
2021-02-25 19:53:57 +0300

    pipes

--- bird-test/GameScene.swift
+++ bird-test/GameScene.swift
@@ -10,12 +10,17 @@ import GameplayKit
 
 class GameScene: SKScene {
     var backgnode : SKNode!
+    var testbirdNode : SKSpriteNode!
+
     override func didMove(to view: SKView) {
         backgnode = self.childNode(withName: "bgnode")!
+        testbirdNode = self.childNode(withName: "testbird") as! SKSpriteNode
         let moveBG = SKAction.move(by:CGVector(dx: -1000, dy:0), duration:15)
         backgnode.run(moveBG)
     }
     
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        testbirdNode.physicsBody!.applyImpulse(CGVector(dx:0,dy:250)) //250 потому что если меньше то играть невозможно
+    }
 
 }
